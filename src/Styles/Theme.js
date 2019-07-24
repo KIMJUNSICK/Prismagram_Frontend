46bb6e5b23049ea6b37cf92dc67ec1a927d6254e
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-24 10:57:02 +0900

    GlobalStyles and Theme

---
+++ src/Styles/Theme.js
@@ -0,0 +1,19 @@
+const BOX_BORDER = "1px solid #e6e6e6";
+const BORDER_RADIUS = "4px";
+
+export default {
+  bgColor: "#FAFAFA",
+  blackColor: "#262626",
+  redColor: "#ED4956",
+  darkBlueColor: "#003569",
+  blueColor: "#3897f0",
+  lightGreyColor: "#c7c7c7",
+  darkGreyColor: "#999",
+  borderRadius: "4px",
+  boxBorder: "1px solid #e6e6e6",
+  whiteBox: `
+            ${BOX_BORDER};
+            ${BORDER_RADIUS};
+            background-color: white;
+            `
+};
