cc98705282e8e72a64a8a7c01820cbf0b48720ad
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-26 09:20:12 +0900

    Toastify

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -3,14 +3,22 @@ import useInput from "../../Hooks/useInput";
 import AuthPresenter from "./AuthPresenter";
 import { useMutation } from "react-apollo-hooks";
 import { LOG_IN } from "./AuthQueries";
+import { toast } from "react-toastify";
 
 export default () => {
   const [action, setAction] = useState("logIn");
   const username = useInput("");
   const firstName = useInput("");
   const lastName = useInput("");
-  const email = useInput("");
+  const email = useInput("jinaganda5@naver.com");
   const [requestSecret] = useMutation(LOG_IN, {
+    update: (_, { data }) => {
+      const { requestSecret } = data;
+      if (!requestSecret) {
+        toast.error("You don't have an account yet, create one");
+        setTimeout(() => setAction("signUp"), 2000);
+      }
+    },
     variables: { email: email.value }
   });
 
